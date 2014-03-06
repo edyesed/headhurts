64c3458f865635f627874e9f8a37fc30f91fa205
Ed Anderson <edyesed@gmail.com>
2014-03-06 09:06:53 -0600

    who needs ack.rb? not me.

--- ack.rb
+++
@@ -1,9 +0,0 @@
-require 'mongo'
-
-connection = Mongo::Connection.new("localhost")
-connection.database_names.each do |name|
-  db = connection.db(name)
-  db.collections.each do |collection|
-    puts "#{name} - #{collection.name}"
-  end
-end
